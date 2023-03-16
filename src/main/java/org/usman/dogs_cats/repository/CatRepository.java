a3e5397b15d7d4f85797a9f1decca326a70d70b7
USMANKHALID-GH <loftyusman@gmail.com>
2023-03-16 12:46:14 +0300

    cat repo

--- src/main/java/org/usman/dogs_cats/repository/CatRepository.java
+++ src/main/java/org/usman/dogs_cats/repository/CatRepository.java
@@ -1,8 +1,24 @@
 package org.usman.dogs_cats.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.usman.dogs_cats.model.Cat;
+
+
 @Repository
 public interface CatRepository  extends JpaRepository<Cat, Long> {
+
+    @Query("from Cat  c where  c.breed.breedName=:name")
+    Page<Cat> findCatsByBreedName(Pageable pageable, @Param("name") String name);
+
+    @Query(value = """
+   select cat_name, description ,image, price from cat_tbl where availability=1
+   union
+   select dog_name, description ,image, price from dog_tbl where availability=1;
+    """ , nativeQuery = true )
+    Page<Object>  findAllCatAndDogByAvailability(Pageable pageable  );
 }
--- src/main/java/org/usman/dogs_cats/service/CatService.java
+++ src/main/java/org/usman/dogs_cats/service/CatService.java
@@ -1,4 +1,27 @@
 package org.usman.dogs_cats.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.repository.query.Param;
+import org.usman.dogs_cats.model.Cat;
+
+import java.util.List;
+
 public interface CatService {
+    void createCat(Cat toEntity);
+
+    List<Cat> showAllcat(Pageable pageable);
+
+    List<Cat> showAllCatNotAvailable(Pageable pageable);
+
+    Page<Cat> findByCatByBreedName(Pageable pageable, String name);
+
+    Cat findByCatId(Long id);
+
+    void updateCat(Long id, Cat toEntity);
+
+    void deleteCatById(Long id);
+
+    Page<Object>  findAllCatAndDogByAvailability(Pageable pageable  );
 }
